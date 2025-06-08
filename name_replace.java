8d5413a95ac575ca5266f45ecf9eee47c2a132d1
neha <nehamoni180@gmail.com>
2025-06-08 23:06:43 +0600

    Create name_replace.java

---
+++ name_replace.java
@@ -0,0 +1,11 @@
+//character replacement in initialised string 
+package Main;
+
+public class Main{
+ public static void main(String[] args){
+		      String name = "Surya";
+		      String name2 = name.replace('y', 'j');
+		      System.out.println(name2);
+		    
+ }
+ }
