a5a71c61f1359ceb1f2ce81e62f7a7b8f41c23cd
neha <nehamoni180@gmail.com>
2025-06-08 23:09:34 +0600

    Create str_concat.java

---
+++ str_concat.java
@@ -0,0 +1,11 @@
+//simple string concatenation 
+package Main;
+
+public class Main{
+ public static void main(String[] args){
+		      String name1 = "Michael";
+		      String name2 = " DON";
+		      System.out.println(name1 + name2);
+		    
+ }
+ }
