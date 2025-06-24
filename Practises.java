c543eb5433ca2fec32292173ed4de9bad10e6621
neha <nehamoni180@gmail.com>
2025-06-24 12:55:26 +0600

    Add files via upload

--- Practises.java
+++ Practises.java
@@ -1,6 +1,6 @@
 package Theory;
 
-//checking with conditionals if a number is a multiple of 5 or divisor of 2
+//checking a year is leap year or not
 import java.util.Scanner;
 public class Practises {
 	  public static void main(String[] args) {
@@ -13,6 +13,8 @@ public class Practises {
 	    	System.out.print("Leap year !");
 	    }else {
 	    	System.out.print("Not a Leap year !");
+	    	
 	    }
+	    input.close();
 	  }
 }
