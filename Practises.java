97a19db48c2da2505d0339171cb5f2aed9ef4f5f
neha <nehamoni180@gmail.com>
2025-06-24 12:23:48 +0600

    SimpleIF.java

--- Practises.java
+++ Practises.java
@@ -1,20 +1,22 @@
 package Theory;
 
+//checking with conditionals if a number is a multiple of 5 or divisor of 2
 import java.util.Scanner;
 public class Practises {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter an integer : ");
+		System.out.println("Enter an integer : "); //taking integer input 
 		int integer= input.nextInt();
 		
-		if(integer%5 == 0) 
-			System.out.println("HiFive ");	
+		if(integer % 5 == 0)  //multiple of 5
+			System.out.println("HiFive ");	//prints Hifive
 		
-		if(integer%2 == 0) 
-			System.out.println("HiEven");	
+		if(integer%2 == 0) //divisible by 2
+			System.out.println("HiEven");	//Prints HiEven
+		
+		input.close();
 		}
 	}
-
-
