c5964982013c1cc4c86df60493a56b04a195e9b9
neha <nehamoni180@gmail.com>
2025-06-24 12:53:34 +0600

    Add files via upload
    Leap year checking java program

--- Practises.java
+++ Practises.java
@@ -3,20 +3,16 @@ package Theory;
 //checking with conditionals if a number is a multiple of 5 or divisor of 2
 import java.util.Scanner;
 public class Practises {
-
-	public static void main(String[] args) {
-		
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter an integer : "); //taking integer input 
-		int integer= input.nextInt();
-		
-		if(integer % 5 == 0)  //multiple of 5
-			System.out.println("HiFive ");	//prints Hifive
-		
-		if(integer%2 == 0) //divisible by 2
-			System.out.println("HiEven");	//Prints HiEven
-		
-		input.close();
-		}
-	}
+	  public static void main(String[] args) {
+	  
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter a year : ");
+	    int year = input.nextInt();
+	    
+	    if((year%4 == 0 && year%100 !=0) || (year%400 == 0)){
+	    	System.out.print("Leap year !");
+	    }else {
+	    	System.out.print("Not a Leap year !");
+	    }
+	  }
+}
