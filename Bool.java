721118a7722b876aab8e4f4b64e4c5539910d3e7
neha <nehamoni180@gmail.com>
2025-07-20 14:28:41 +0600

    BoolOperatorwithConditionals
    Output:
    Divisors are 1 and that num.
    Euler's number!

---
+++ Bool.java
@@ -0,0 +1,26 @@
+package golly;
+
+public class Bool {
+
+	public static void main(String[] args) {
+		boolean isPrime = true;
+		if(isPrime == true) {
+			System.out.println("Divisors are 1 and that num.");
+		}else {
+			System.out.println("Garbage value");
+			}
+		
+		double Constant = 2.71;
+		if(Constant>2.71 && Constant<4) {
+			System.out.println("Pi");
+		}else {
+			System.out.println("Euler's number! ");
+		}
+		
+		
+		}
+		
+
+	}
+
+
