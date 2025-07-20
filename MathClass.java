d0965bc4c2ec874e1fb9e0fb399f7dc6c20435bc
neha <nehamoni180@gmail.com>
2025-07-20 13:04:42 +0600

    MathClass.java

---
+++ MathClass.java
@@ -0,0 +1,20 @@
+package constructors;
+
+public class OverloadDemo {
+
+	public static void main(String[] args) {
+		double a = 1;
+		int b = 2;
+		double rem = a%b;
+		
+		int num = 1 ;
+		// System.out.println( rem);
+
+		// System.out.println( -- num);
+		// System.out.println( ++ num);
+		
+		// math class functions 
+		System.out.print(Math.max(a, b)); //prints the max value 
+	}
+
+}
